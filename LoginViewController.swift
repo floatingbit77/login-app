90dd3892d4e29f9e21890cdeec50ff6f536eb7fa
Abi <71341663+floatingbit77@users.noreply.github.com>
2021-02-03 00:48:14 -0500

    Update LoginViewController.swift

--- LoginViewController.swift
+++ LoginViewController.swift
@@ -37,16 +37,6 @@ class LoginViewController: UIViewController {
         Utilities.styleFilledButton(loginButton)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
     @IBAction func loginTapped(_ sender: Any) {
         
         //TODO: validate text fields
